4fad1104e52a34e14b5d26e49c95cb77d7d1cadf
unatalbarbara <unatalbarbara@gmail.com>
2020-03-28 22:47:58 +0100

    idk gurl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,11 @@
 package com.company;
-
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Main {
+    private static Scanner teclado = new Scanner(System.in);//Variable global
+
     static void MasterMind() {
-        Scanner teclado = new Scanner(System.in);
+
         System.out.print("Escoje un numero de letras de donde seleccionar, por ejemple (2-15):");
         int charToSelect = teclado.nextInt();
 
@@ -16,31 +16,45 @@ public class Main {
         System.out.print("Numero maximo de jugadas antes de terminar la partida: ");
         int partidasNumber = teclado.nextInt();
 
-
         char[] Colors = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'};
-        char retorna[] = new char[charToSelect];
+
+        char[] retorna = new char[charToSelect];
         for (int i = 0; i < retorna.length; i++) {
             retorna[i] = Colors[i];
-        }
+        }//CREACION DE OPCIONES
 
         System.out.println("Tus letras son: " + Arrays.toString(retorna));
-        int contador = 1;
-        System.out.print("Jugada " + contador + ":");
-
-        int miJugada = teclado.nextInt();
-
-        fillArray(retorna, letrasJugar, partidasNumber);//Hey
+        //COMENZAR A JUGAR
+        int contador = 0;
+        while (partidasNumber <= contador) {
+            InputJugador();
+            contador++;
+        }
     }
 
-    public static char[] fillArray(char toSelect[], int inputJugada, int Cuatesjugadas) {
-        char[] copyV = Arrays.copyOf(toSelect, toSelect.length);//Ineficiente Borrar
-        char Tablero[] = new char[inputJugada * Cuatesjugadas];
+
+    //Rellenar lo datos de la partida, el numero de jugadas y creacion del tablero
+    public static char[] fillArray(char toSelect[], int inputJugada, int Cuantasjugadas) {
+//        char[] copyV = Arrays.copyOf(toSelect, toSelect.length);//Ineficiente Borrar//Creoquehayquecopiar
+//        char[] copyV = Arrays.copyOf(toSelect, toSelect.length);//Ineficiente Borrar//Creoquehayquecopiar
+        char Tablero[] = new char[inputJugada * Cuantasjugadas];
         for (int i = 0; i < Tablero.length; i++) {
             Tablero[i] += toSelect[(int) (Math.random() * toSelect.length)];
         }
         return Tablero;
     }
 
+    public static void InputJugador() {
+        System.out.print("Jugada :");
+        String miJugada = teclado.nextLine();
+        char[] miJugadaA = new char[miJugada.length()];
+        for (int i = 0; i < miJugada.length(); i++) {
+            miJugadaA[i] = miJugada.charAt(i);
+        }
+        System.out.println(Arrays.toString(miJugadaA));
+    }
+
+    //Comparar si son validas las posiciones o no, fichas negras y blancas.
     public static void juego(char tablero[], char input[]) {//Posiciones iguales, retorna las negras
         for (int i = 0; i < input.length; i++) {
             if (tablero[i] == input[i]) {
@@ -57,8 +71,9 @@ public class Main {
 
     public static void main(String[] args) {
 //        MasterMind();
-        char[] Colors = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',};
-        char[] jugada = {'A', 'B', 'D', 'D'};
-        juego(Colors, jugada);
+//        char[] Colors = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',};
+//        char[] jugada = {'A', 'B', 'C', 'D'};
+
     }
+
 }

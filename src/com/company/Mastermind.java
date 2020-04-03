4c698616419c9eea6cc7bafdf6682e6b5b71d738
unatalbarbara <unatalbarbara@gmail.com>
2020-04-03 03:52:01 +0200

    game

--- src/com/company/Mastermind.java
+++ src/com/company/Mastermind.java
@@ -1,84 +1,84 @@
-package com.company;
-
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.*;
-
-public class Mastermind {
-    public static int contador = 0;
-
-    public static void MasterMind() {
-
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("Escoje un numero de letras de donde seleccionar, por ejemple (2-6):");
-        int charToSelect = teclado.nextInt();
-
-        System.out.print("Escoje un numero de letras para jugar por ejemple (1-6):");
-        int letrasJugar = teclado.nextInt();
-
-        System.out.print("Numero maximo de jugadas antes de terminar la partida: ");
-        int partidasNumber = teclado.nextInt();
-
-        char[] Colors = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'};
-        char[] retorna = new char[charToSelect];
-        for (int i = 0; i < retorna.length; i++) {
-            retorna[i] = Colors[i];
-        }
-        System.out.println("Tus letras son: " + Arrays.toString(retorna));
-        juego(tablero(retorna, letrasJugar), inputJugada(), partidasNumber);
-    }
-
-    public static char[] inputJugada() {
-        Scanner teclado = new Scanner(System.in);
-        contador++;
-        System.out.println("Jugada " + contador + " : ");
-        String jugada = teclado.nextLine();
-        char[] miJugada = new char[jugada.length()];
-        for (int i = 0; i < jugada.length(); i++) {
-            miJugada[i] = jugada.charAt(i);
-        }
-        System.out.println(Arrays.toString(miJugada));
-        return miJugada;
-    }
-
-    public static char[] tablero(char v[], int jugada) {
-        char[] tablero = new char[jugada];
-        for (int i = 0; i < tablero.length; i++) {
-            tablero[i] += v[(int) (Math.random() * v.length)];
-        }
-        System.out.println("este es el tablero" + Arrays.toString(tablero));
-        return tablero;
-    }
-
-    public static void juego(char tablero[], char jugada[], int numPartidas) {
-        int blancas = 0;
-        int negras = 0;
-        for (int i = 0, j = 0; i < jugada.length; i++) {
-            if (tablero[i] == jugada[i]) {
-                blancas++;
-            } else if (tablero[j] == jugada[i]) {
-                negras++;
-                j++;
-            }
-        }
-        if (blancas >= 1 && blancas != jugada.length) {
-            System.out.print(blancas + "B ");
-        } else if (negras >= 1) {
-            System.out.print(blancas + "B ");
-        } else if (blancas == jugada.length) {
-            System.out.println("Felicidades, has encontrado la combinacion en el intento " + contador);
-        }
-        if (numPartidas > 1 && blancas != jugada.length) {
-            numPartidas--;
-            juego(tablero, inputJugada(), numPartidas);
-        } else if (numPartidas == 1) {
-            System.out.println("Lo siento, no has acerdado, intenta jugar otra vez");
-        }
-
-
-    }
-
-    public static void main(String[] args) {
-        MasterMind();
-    }
-}
+//package com.company;
+//
+//import java.util.Arrays;
+//import java.util.Scanner;
+//import java.util.*;
+//
+//public class Mastermind {
+//    public static int contador = 0;
+//
+//    public static void MasterMind() {
+//
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.print("Escoje un numero de letras de donde seleccionar, por ejemple (2-6):");
+//        int charToSelect = teclado.nextInt();
+//
+//        System.out.print("Escoje un numero de letras para jugar por ejemple (1-6):");
+//        int letrasJugar = teclado.nextInt();
+//
+//        System.out.print("Numero maximo de jugadas antes de terminar la partida: ");
+//        int partidasNumber = teclado.nextInt();
+//
+//        char[] Colors = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'};
+//        char[] retorna = new char[charToSelect];
+//        for (int i = 0; i < retorna.length; i++) {
+//            retorna[i] = Colors[i];
+//        }
+//        System.out.println("Tus letras son: " + Arrays.toString(retorna));
+//        juego(tablero(retorna, letrasJugar), inputJugada(), partidasNumber);
+//    }
+//
+//    public static char[] inputJugada() {
+//        Scanner teclado = new Scanner(System.in);
+//        contador++;
+//        System.out.println("Jugada " + contador + " : ");
+//        String jugada = teclado.nextLine();
+//        char[] miJugada = new char[jugada.length()];
+//        for (int i = 0; i < jugada.length(); i++) {
+//            miJugada[i] = jugada.charAt(i);
+//        }
+//        System.out.println(Arrays.toString(miJugada));
+//        return miJugada;
+//    }
+//
+//    public static char[] tablero(char v[], int jugada) {
+//        char[] tablero = new char[jugada];
+//        for (int i = 0; i < tablero.length; i++) {
+//            tablero[i] += v[(int) (Math.random() * v.length)];
+//        }
+//        System.out.println("este es el tablero" + Arrays.toString(tablero));
+//        return tablero;
+//    }
+//
+//    public static void juego(char tablero[], char jugada[], int numPartidas) {
+//        int blancas = 0;
+//        int negras = 0;
+//        for (int i = 0, j = 0; i < jugada.length; i++) {
+//            if (tablero[i] == jugada[i]) {
+//                blancas++;
+//            } else if (tablero[j] == jugada[i]) {
+//                negras++;
+//                j++;
+//            }
+//        }
+//        if (blancas >= 1 && blancas != jugada.length) {
+//            System.out.print(blancas + "B ");
+//        } else if (negras >= 1) {
+//            System.out.print(blancas + "B ");
+//        } else if (blancas == jugada.length) {
+//            System.out.println("Felicidades, has encontrado la combinacion en el intento " + contador);
+//        }
+//        if (numPartidas > 1 && blancas != jugada.length) {
+//            numPartidas--;
+//            juego(tablero, inputJugada(), numPartidas);
+//        } else if (numPartidas == 1) {
+//            System.out.println("Lo siento, no has acerdado, intenta jugar otra vez");
+//        }
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        MasterMind();
+//    }
+//}
--- src/com/company/finalC.java
+++ src/com/company/finalC.java
@@ -1,38 +1,36 @@
 package com.company;
-
 import java.util.*;
-
 public class finalC {
-    private static int lletresEscollir; // Número de lLetres entre les que podem escollir  (Ex: 6)
-    private static int lletresJugar;    // Número de lletres que jugarem   (Ex: 4)
-    private static int numJugada = 1;   // Número de la jugada actual, 1 per a la primera jugada
-    private static int jugadasMax;      // Número màxim de jugades que farem
-    private static boolean repetirLletres = false;      // No es deixen repetir lletres a la combinació (o si)
+    private static int lletresJugar;
+    private static int numJugada = 1;
+    private static int jugadasMax;
+    private static boolean repetirLletres = false;
     private static char[] lletres = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'};
-    private static char[] jugada;      // Manté un array amb la jugada actual	(Ex:  [A,C,A,F]
-    private static char[] pensat;      // Manté un array amb la combinació que ha pensat la màquina (Ex: [B,A,C,A])
+    private static char[] jugada;
+    private static char[] pensat;
+    private static int lletresEscollir;
 
-
-    private static boolean contingudes(char[] lletres, String s) {
+    private static boolean contingudes(char[] lletres, String s) {//Comprobar inputs
         String cadena = passaCadena(lletres);
         return cadena.contains(s);
     }
 
     private static char[] pensaLletres(char[] lletres, int quantes) {
         String string = passaCadena(lletres);
+        string = string.substring(0, quantes);
         List<String> letters = Arrays.asList(string.split(""));
         Collections.shuffle(letters);
         String shuffled = "";
         for (String letter : letters) {
             shuffled += letter;
         }
-        return passaVector(shuffled.substring(0, quantes));
+        return passaVector(shuffled);
     }
 
     private static char[] pensaRepetides(char[] lletres, int quantes) {
         char[] tablero = new char[quantes];
         for (int i = 0; i < tablero.length; i++) {
-            tablero[i] += lletres[(int) (Math.random() * lletres.length)];
+            tablero[i] += lletres[(int) (Math.random() * quantes)];
         }
         return tablero;
     }
@@ -40,19 +38,23 @@ public class finalC {
     private static void llegeixDades() {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Escoje un numero de letras de donde seleccionar, por ejemple (2-6):");
-        int lletresJugar = teclado.nextInt();
+//        int lletresJugar = teclado.nextInt();
+        lletresJugar = 4;
         System.out.print("Escoje un numero de letras para jugar por ejemple (1-6):");
-        int lletresEscollir = teclado.nextInt();
+//        int lletresEscollir = teclado.nextInt();
+        lletresEscollir = 4;
         System.out.print("Numero maximo de jugadas antes de terminar la partida: ");
-        int jugadasMax = teclado.nextInt();
+//        int jugadasMax = teclado.nextInt();
+        jugadasMax = 12;//NO SE EJECUTAN LAS 12
+
     }
 
     static char[] passaVector(String s) {
-        char[] miJugada = new char[s.length()];
+        jugada = new char[s.length()];
         for (int i = 0; i < s.length(); i++) {
-            miJugada[i] = s.charAt(i);
+            jugada[i] = s.charAt(i);
         }
-        return miJugada;
+        return jugada;
     }
 
     static String passaCadena(char[] lletres) {
@@ -60,61 +62,54 @@ public class finalC {
         return v;
     }
 
-    // // Pide al usuario que introduzca la próxima jugada, y la compara con la jugada que ha pensado
-// // el programa. Le dice al usuario las letras que tiene bien y las que tiene regular (está la letra pero
-// // mal puesta)
-// // Incrementa el número de jugadas, y si el usuario acierta todas las letras, le felicita y devuelve
-// // true, indicando que se ha terminado la partida. En caso contrario, devuelve false para continuar jugando.
     static boolean properaJugada() {
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("\n Jugada " + numJugada + " : ");
-        String s = teclado.nextLine();
+//        Scanner teclado = new Scanner(System.in);
+        System.out.print("\n Jugada " + numJugada + " : ");//        String s = teclado.nextLine();
+        String s = "ABCD";
+        System.out.println(s);
         jugada = passaVector(s);
-        int B = 0;
-        int R = 0;
-        for (int i = 0, j = 0; i < jugada.length; i++) {
+        int B = 0, R = 0;
+        for (int i = 0; i < jugada.length; i++) {
             if (pensat[i] == jugada[i]) {
                 B++;
-            } else if (pensat[j] == jugada[i]) {
-                R++;
-                j++;
+            } else {
+                for (int k = 0; k < jugada.length; k++) {
+                    if (jugada[i] == pensat[k])
+                        R++;
+                }
             }
         }
+        numJugada++;
         if (B >= 1 && B != jugada.length) {
             System.out.print(B + "B ");
-        } else if (R >= 1) {
-            System.out.print(B + "B ");
+        }
+        if (R >= 1) {
+            System.out.print(R + "R ");
         } else if (B == jugada.length) {
             System.out.println("Felicidades, has encontrado la combinacion en el intento " + numJugada);
             return true;
         }
-        if (numJugada > 1 && B != jugada.length) {
-            numJugada--;
-            properaJugada();
-        } else if (numJugada == 1) {
-            System.out.println("Lo siento, no has acerdado, intenta jugar otra vez");
-            return false;
-        }
-
         return false;
     }
-
 //    private static int llegeixInt(String missatge, int min, int max) {
 //
 //    }
 
     public static void main(String[] args) {
-        // Programa principal, s'indica complet a falta de les funcions
         llegeixDades();
         if (!repetirLletres)
             pensat = pensaLletres(lletres, lletresJugar);
         else
             pensat = pensaRepetides(lletres, lletresJugar);
         // Si volem saber les lletres que s'han pensat, descomentar
+        System.out.println(" ");
         System.out.println(java.util.Arrays.toString(pensat));
-        while (!properaJugada() && numJugada <= jugadasMax) ;
+        while (!properaJugada() && numJugada <= jugadasMax) {
+//            jugadasMax--;
+            properaJugada();
+        }
         if (numJugada > jugadasMax)
-            System.out.println("Ho sento, no l'has encertat.  Torna a jugar");
+            System.out.println("\nHo sento, no l'has encertat.  Torna a jugar");
     }
 }
 
